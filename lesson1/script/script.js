2beb2af8ef1b8ff231edea1d5a6020d52915aba1
KaminskyVova <kamok@bk.ru>
2020-01-23 18:16:27 +0200

    first commit

---
+++ lesson1/script/script.js
@@ -0,0 +1,13 @@
+
+let money = 20000; 
+let income = 30; 
+let addExpenses = 100; 
+let deposit = 20500; 
+let mission = 'Hello';
+let period = '3 manth';
+
+console.log(period);
+
+alert(mission);
+
+console.log(income + money);
