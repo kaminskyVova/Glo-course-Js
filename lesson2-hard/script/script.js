1d4a24cb2ff20f7e27be3a5d26e4e7f116895614
KaminskyVova <kamok@bk.ru>
2020-01-26 13:58:54 +0200

    added files to lesson2-Hard

---
+++ lesson2-hard/script/script.js
@@ -0,0 +1,42 @@
+// присовить значения переменным
+// let money = 30000;
+// let income = 'Фриланс';
+// let deposit = true;
+// let mission = 100000 + ' баксов';
+// let period = 12;
+// let addExpenses = 'Жилье ' + 'Транспорт ' + 'Еда ' + 'Гульки-Шмульки';
+
+// // методы и свойства
+// console.log(typeof money);
+// console.log(typeof icome);
+// console.log(typeof deposit);
+// console.log(addExpenses.length);
+// console.log('Период ' + 'равен ' + period + ' ' + 'месяцев');
+// console.log('Цель ' + 'заработать ' + mission);
+// console.log(addExpenses.toLocaleLowerCase().split(' '));
+// let budgetDay = 2500;
+// console.log(budgetDay * 30 + ' руб ' + 'нужно на месяц');
+
+
+// усложненный
+
+let num = +'266219';
+console.log(typeof num);
+// console.log(num);
+
+// не понял задание!!!
+// 2) Вывести в консоль произведение (умножение) цифр этого числа
+// Например: число 123, при помощи javaScript получить каждое цифру ( 1, 2, 3 ) и перемножить их.
+// Правильно использовать цикл или методы перебора.
+
+console.log((2 * 6 * 6 * 2 * 1 * 9) ** 3);
+
+num = [2, 6, 6, 2, 1, 9];
+console.log(num);
+num = '266219';
+
+// найти оператор перевода числа в строку!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (как мин 2 спосб)
+// что бы перевести число в строку надо число завернуть "", а если строку в число то перед строкой поставить +;
+
+console.log(num.length);
+console.log(num.slice(0, 2));
