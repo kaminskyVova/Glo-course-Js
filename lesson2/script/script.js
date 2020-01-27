079e318c3079ffa4ce8c0041b0a42d0f450193cc
KaminskyVova <kamok@bk.ru>
2020-01-27 13:46:40 +0200

    правки по ДЗ один момент не углядел добавил

--- lesson2/script/script.js
+++ lesson2/script/script.js
@@ -12,7 +12,7 @@ console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
-console.log('Цель ' + 'заработать ' + mission);
+console.log('Цель заработать ' + mission);
 console.log(addExpenses.toLocaleLowerCase().split(','));
 let budgetDay = (money / 30);
 console.log(budgetDay);
