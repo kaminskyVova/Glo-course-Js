2c2cadc0693551ef6ba5f5840c68881b8957e89f
KaminskyVova <kamok@bk.ru>
2020-01-27 15:19:00 +0200

    последние правки-поправки

--- lesson2/script/script.js
+++ lesson2/script/script.js
@@ -13,6 +13,9 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission);
-console.log(addExpenses.toLocaleLowerCase().split(','));
+//////
+addExpenses = addExpenses.toLocaleLowerCase().split(', ');
+console.log(addExpenses);
+////////
 let budgetDay = (money / 30);
 console.log(budgetDay);
