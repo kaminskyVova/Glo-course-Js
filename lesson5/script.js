c9c717aaa58e6a095713ed380cf34115eec05241
KaminskyVova <kamok@bk.ru>
2020-02-02 06:31:02 +0200

    добавил поправки к ДЗ-5

--- lesson5/script.js
+++ lesson5/script.js
@@ -5,21 +5,21 @@
 ////////////////////////////////////
 
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 let money = 0, // доход за месяц
-    income = 'Фриланс', // доп доход
-    deposit = true, // депозит
-    mission = 1000000, // цель
-    period = 12, // желательный период
-    addExpenses = 0, // обязательные расходы
-    expenses = []; // статьи расходов
+  income = 'Фриланс', // доп доход
+  deposit = true, // депозит
+  mission = 1000000, // цель
+  period = 12, // желательный период
+  addExpenses = 0, // обязательные расходы
+  expenses = []; // статьи расходов
 
 
 // определение типов данных
 function showTypeOf(a) {
-    console.log(typeof a);
+  console.log(typeof a);
 };
 
 showTypeOf(income);
@@ -29,29 +29,28 @@ showTypeOf(deposit);
 
 // месячный доход
 let start = function () {
-    do {
-        money = prompt('Ваш месячный доход?');
-    }
-    while (!isNumber(money))
+  do {
+    money = prompt('Ваш месячный доход?');
+  }
+  while (!isNumber(money))
 
-    // // while (isNaN(money) || money.trim() === '' || money === null) {
-    // // while (isNaN(parseFloat(money)))
-    // while (!isNumber(money)){
-    //     // money = prompt('Ваш месячный доход?');
-    //  }
+  // // while (isNaN(money) || money.trim() === '' || money === null) {
+  // // while (isNaN(parseFloat(money)))
+  // while (!isNumber(money)){
+  //     // money = prompt('Ваш месячный доход?');
+  //  }
 };
-start();
-
+Math.ceil(start());
 
 //считаем обязательные расходы
 let getExpensesMonth = function () {
 
-    for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 2; i++) {
 
-        expenses[i] = prompt('Введите обязательную статью расходов - 1', 'Название');
+    expenses[i] = prompt('Введите обязательную статью расходов', 'Название');
 
-        addExpenses += +prompt('Во сколько это обойдется?');
-    }
+    addExpenses += +prompt('Во сколько это обойдется?');
+  }
 
 };
 getExpensesMonth();
@@ -63,7 +62,8 @@ showTypeOf(addExpenses);
 
 // остаток после расходов
 let accumulatedMonth = function getAccumulatedMonth() {
-    accumulatedMonth = (money - addExpenses);
+  accumulatedMonth = (money - addExpenses);
+  return accumulatedMonth;
 };
 accumulatedMonth();
 console.log(accumulatedMonth);
@@ -71,36 +71,37 @@ console.log(accumulatedMonth);
 
 // дневной бюджет
 let budgetDay = function getBudgetDay(a) {
-    budgetDay = (Math.floor(a / 30));
-    return budgetDay;
+  budgetDay = (Math.floor(a / 30));
+  return budgetDay;
 };
 console.log(budgetDay(accumulatedMonth));
 
 
 // анализ доходов
 function getStatusIncome() {
-    if (budgetDay >= 1200) {
-        return ('У вас высокий уровень дохода');
-    } else if (budgetDay > 600 && budgetDay < 1200) {
-        return ('У вас средний уровень дохода');
-    } else if (budgetDay <= 600 && budgetDay >= 0) {
-        return ('К сожалению у вас уровень дохода ниже среднего');
-    } else if (budgetDay < 0) {
-        return ('Что то пошло не так');
-    }
+  if (budgetDay >= 1200) {
+    return ('У вас высокий уровень дохода');
+  } else if (budgetDay > 600 && budgetDay < 1200) {
+    return ('У вас средний уровень дохода');
+  } else if (budgetDay <= 600 && budgetDay >= 0) {
+    return ('К сожалению у вас уровень дохода ниже среднего');
+  } else if (budgetDay < 0) {
+    return ('Что то пошло не так');
+  }
 };
 console.log(getStatusIncome());
 
 
 // срок достижение целм
 function getTargetMonth() {
-    let sum = Math.ceil((mission / accumulatedMonth));
+  let sum = Math.ceil(mission / accumulatedMonth);
 
-    if (sum < 0) {
-        console.log('Цель не будет достигнута');
-    } else {
-        console.log('Цель будет достигнута за: ' + Math.ceil((mission / accumulatedMonth)) + ' месяцев');
-    }
+  if (sum < 0) {
+    console.log('Цель не будет достигнута');
+  } else {
+    console.log('Цель будет достигнута за: ' + Math.ceil(sum) + ' месяцев');
+  }
+  return sum;
 
 };
 getTargetMonth();
