8776c737c22ecded023eb5894b2147c7f596c3e4
KaminskyVova <kamok@bk.ru>
2020-02-24 14:03:02 +0200

    дз-10 книги

--- lesson10/script.js
+++ lesson10/script.js
@@ -11,11 +11,11 @@ body.setAttribute('style', 'background: url(./image/adv.jpg)');
 
 // получаем родителя списков
 let books = document.querySelector('.books');
-console.log(books);
+// console.log(books);
 
 // получаем все книги
 let booksList = document.querySelectorAll('.book');
-console.log(booksList);
+// console.log(booksList);
 
 //меняем порядок книг
 books.insertBefore(booksList[1], booksList[0]);
@@ -25,18 +25,42 @@ books.insertBefore(booksList[2], null);
 
 //меняем заголовок 3й книги
 let book3 = booksList[4].querySelector('a');
-console.log(book3);
+// console.log(book3);
 book3.innerHTML = 'Книга 3. this и Прототипы Объектов';
 
 
 // Меняем порядок глав во 2й книге
-console.log(booksList[0]);
 
-//получили лишки книги
-let book_2 = booksList[0].querySelectorAll('li');
-console.log(book_2);
+//получили лишки книги 2
+let book_2 = booksList[0].querySelectorAll("ul"),
+    book_2List = booksList[0].querySelectorAll("li");
+console.log(book_2[0]);
 
-// меняем порядок глав 2й книги
-booksList[0].insertBefore(book_2[5], null);
-console.log(book_2[4]);
-// booksList[0].insertBefore(book_2[6], book_2[4]);
+book_2[0].insertBefore(book_2List[3], book_2List[2]);
+book_2[0].insertBefore(book_2List[6], book_2List[2]);
+book_2[0].insertBefore(book_2List[8], book_2List[2]);
+book_2[0].insertBefore(book_2List[4], book_2List[2]);
+book_2[0].insertBefore(book_2List[5], book_2List[2]);
+book_2[0].insertBefore(book_2List[2], book_2List[10]);
+
+
+// Меняем порядок глав во 5й книге
+//получили лишки книги 5
+
+let book_5 = booksList[5].querySelectorAll("ul"),
+    book_5List = booksList[5].querySelectorAll("li");
+
+book_5[0].insertBefore(book_5List[9], book_5List[2]);
+book_5[0].insertBefore(book_5List[3], book_5List[2]);
+book_5[0].insertBefore(book_5List[4], book_5List[2]);
+book_5[0].insertBefore(book_5List[5], book_5List[8]);
+
+
+
+//добавляем главу в 6ю книгу
+let book_6 = booksList[2].querySelectorAll('li');
+// добавляем новый элемент li
+let newLi = document.createElement('li');
+newLi.textContent = 'Глава 8: За пределами ES6';
+booksList[2].appendChild(newLi);
+booksList[2].insertBefore(book_6[9], null);
--- lesson12/script.js
+++ lesson12/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /////////////////////////////////////////////
-//////////////урок одинадцатый///////////////////
+//////////////урок двенадцатый///////////////////
 ///////////////////////////////////////////
 
 let isNumber = function (n) {
@@ -134,19 +134,6 @@ const appData = {
 
         // appData.showResult();
 
-
-
-        // // вызовы методов
-        // appData.start(); //месячный доход
-        // appData.getMission(); // сколько хотят накопить
-        // appData.getInfoDeposit() // наличие сумма и процент по депозиту
-        // appData.asking(); // опросник
-        // appData.getExpensesMonth(); // обязательные расходы
-        // appData.getBudget(); // остаток после расходов
-        // appData.getStatusIncome(); //  анализ доходов
-        // console.log(appData.getStatusIncome()); // анализ доходов(консоль)
-        // appData.getTargetMonth(); // срок достижение цели
-        // console.log(appData); // объект
     },
 
     //вывод доходов месяц день расходы на месяц
@@ -252,17 +239,11 @@ const appData = {
     },
 
 
-    ////запрос сколько хотят накопить
-    getMission: function () {
+    // ////запрос сколько хотят накопить
+    // getMission: function () {
 
-        // do {
-        //     appData.mission = +prompt('Сколько вы хотите отложить?', 500000);
-        // }
-        // while (!isNumber(appData.mission))
-        // appData.mission = +appData.mission;
 
-        // console.log('Ваша цель накопить: ' + appData.mission);
-    },
+    // },
 
 
     // обязательные расходы
@@ -317,18 +298,10 @@ const appData = {
 
 
 
-    // анализ доходов
-    getStatusIncome: function () {
-        // if (appData.budgetDay >= 1200) {
-        //     return ('У вас высокий уровень дохода');
-        // } else if (appData.budgetDay > 600 && appData.budgetDay < 1200) {
-        //     return ('У вас средний уровень дохода');
-        // } else if (appData.budgetDay <= 600 && appData.budgetDay >= 0) {
-        //     return ('К сожалению у вас уровень дохода ниже среднего');
-        // } else if (appData.budgetDay < 0) {
-        //     return ('Что то пошло не так');
-        // }
-    },
+    // // анализ доходов
+    // getStatusIncome: function () {
+
+    // },
 
 
     // срок достижение цели
@@ -346,69 +319,6 @@ const appData = {
         return missionPeriod;
     },
 
-
-    // опрос клиента
-    // asking: function () {
-    //     // let cashIncome,
-    //     itemIncome;
-
-    //     if (confirm('Есть ли у вас дополнительный заработок?')) {
-
-    //         // доп заработки
-    //         do {
-    //             itemIncome = prompt('Назовите допольнительный заработок?', 'Грабеж, Рэкет, Заказуха').trim();
-    //         }
-    //         while (itemIncome === '')
-
-    //         // заработок на них
-    //         do {
-    //             cashIncome = +prompt('Сколько вам приносит дополнительный заработок?', 20000);
-    //         }
-    //         while (!isNumber(cashIncome))
-    //         appData.income[itemIncome] = cashIncome;
-
-    //         console.log(appData.income);
-    //     };
-
-
-    // обязательные расходы
-    // let key = '',
-    //     mon = '';
-
-    // for (let i = 0; i < 2; i++) {
-
-    //     // статьи расходов
-    //     do {
-    //         key = prompt('Введите обязательную статью расходов', 'Подкуп ментов').trim();
-    //     }
-    //     while (key === '')
-
-
-    //     // растраты на них
-    //     do {
-    //         mon = +prompt('Во сколько это обойдется?', 10000);
-    //     }
-    //     while (!isNumber(mon))
-    //     appData.expenses[key] = +mon;
-
-    // };
-
-    // console.log('Обязательные расходы: ');
-    // console.log(appData.expenses);
-
-    // доп расходы
-
-    // let costs = '';
-    // do {
-    //     costs = prompt('Введите дополнительные статьи расходов', 'водка, бабы, опохмел').trim();
-    // }
-    // while (costs === '')
-
-    // appData.addExpenses = costs;
-    // console.log(appData.addExpenses);
-    // console.log('Дополнительные растраты: ' + costs);
-
-    // },
 
 
     // наличие сумма и процент по депозиту
@@ -435,7 +345,8 @@ const appData = {
 
         console.log('Депозит в банке: ' + appData.moneyDeposit);
         console.log('Процент по дипозиту: ' + appData.percentDeposit);
-    }
+    },
+
 };
 
 
@@ -453,45 +364,10 @@ salaryAmount.addEventListener('input', function () {
 });
 
 
+
+
 appData.getPeriodSelect();
 periodSelect.addEventListener('input', appData.getPeriodSelect.bind(appData));
 startBtn.addEventListener('click', appData.start.bind(appData));
 incomePlus.addEventListener('click', appData.addIncomeBlock.bind(appData));
 expensesPlus.addEventListener('click', appData.addExpensesBlock.bind(appData));
-
-// periodSelect.addEventListener('input', appData.getPeriodSelect.bind(appData));
-// startBtn.addEventListener('click', appData.start.bind(appData));
-// incomePlus.addEventListener('click', appData.addIncomeBlock.bind(appData));
-// expensesPlus.addEventListener('click', appData.addExpensesBlock.bind(appData));
-
-
-
-
-// раскладуем объект
-// разбиваем на ключи -- свойства
-// for (let key in appData) {
-//     console.log('Наша программа включает в себя данные: ' + key + ' - ' + appData[key]);
-// };
-
-
-
-
-/////////////////////////////////////////////////////////////////////////
-// let k = 0,
-//     result = '';
-
-// function firstLetterst() {
-//     for (var i = 0; i < appData.addExpenses.length; i++) {
-//         if (isNaN(appData.addExpenses[i]) && k == 0) {
-//             result += appData.addExpenses.charAt(i).toUpperCase();
-//             k += 1;
-//         } else if (isNaN(appData.addExpenses[i]) && appData.addExpenses[i - 1] == ' ') {
-//             result += appData.addExpenses.charAt(i).toUpperCase();
-//         } else {
-//             result += appData.addExpenses[i];
-//         }
-//     }
-//     console.log(result);
-// }
-// firstLetterst();
-/////////////////////////////////////////////////////
